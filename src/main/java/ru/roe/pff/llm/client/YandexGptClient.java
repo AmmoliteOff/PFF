e8b04f7b60a2c5683f65f74fd95bd88d7b604108
ezhidovlenkov <ezhidovlenkov@sitronics.com>
2024-11-29 21:23:46 +0300

    feat[KT-4]: LLM

--- src/main/java/ru/roe/pff/llm/client/YandexGptClient.java
+++ src/main/java/ru/roe/pff/llm/client/YandexGptClient.java
@@ -3,14 +3,12 @@ package ru.roe.pff.llm.client;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import ru.roe.pff.llm.dto.PromtResponseDto;
 import ru.roe.pff.llm.utils.Promt;
 
-@FeignClient(name = "yandex-gpt-client", url = "https://iam.api.cloud.yandex.net")
+@FeignClient(name = "yandex-gpt-client", url = "https://llm.api.cloud.yandex.net")
 public interface YandexGptClient {
-    @PostMapping("/iam/v1/tokens")
-    void getToken();
-
     @PostMapping("/foundationModels/v1/completion")
-    PromtResponseDto producePromt(@RequestBody Promt promt);
+    PromtResponseDto producePromt(@RequestBody Promt promt, @RequestHeader(name = "Authorization") String token);
 }
--- src/main/java/ru/roe/pff/llm/service/LLMService.java
+++ src/main/java/ru/roe/pff/llm/service/LLMService.java
@@ -2,15 +2,21 @@ package ru.roe.pff.llm.service;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import ru.roe.pff.llm.client.YandexGptClient;
+import ru.roe.pff.llm.client.YandexIamClient;
+import ru.roe.pff.llm.client.YandexProperties;
 import ru.roe.pff.llm.dto.PromtItemDto;
 import ru.roe.pff.llm.dto.PromtResponseDto;
 import ru.roe.pff.llm.utils.Promt;
 import ru.roe.pff.processing.DataRow;
+import ru.roe.pff.processing.LlmWarnings;
 
+import java.lang.reflect.Type;
 import java.util.List;
 import java.util.Random;
 
@@ -19,42 +25,44 @@ import java.util.Random;
 public class LLMService {
     @Value("${application.llm.coverage}")
     private Double coveragePercentage;
+    @Value("${yandex.yandexPassportOauthToken}")
+    private String oAuthToken;
+    @Value("${yandex.uri}")
+    private String modelUri;
     private final Random random = new Random();
     private final YandexGptClient yandexGptClient;
+    private final YandexIamClient yandexIamClient;
     private final ObjectMapper objectMapper;
 
-    public PromtResponseDto checkForTitleChange(List<DataRow> rows) throws JsonProcessingException {
+    public List<LlmWarnings> checkForTitleChange(List<DataRow> rows) throws JsonProcessingException {
         var items = rows.parallelStream()
             .filter(row -> random.nextDouble() < coveragePercentage)
-            .map(row -> new PromtItemDto(row.getData().get(1), row.getIndex(), 1))
             .toList();
-        var promt = Promt.createPrompt(
+        var promt = Promt.createPrompt(modelUri,
             0.4,
             2000,
-            "Вы должны обработать входной JSON-список, где каждый элемент имеет структуру:\n" +
-                "\n" + "public record PromtItemDto (String value, Integer rowIndex, Integer columIndex);\n" +
-                "Ваша задача — проверить поле value каждого элемента. Если оно содержит название продукта с ошибкой " +
-                "(опечаткой), исправьте его на правильное. Если название корректное или его невозможно однозначно " +
-                "исправить, оставьте его без изменений.\n" +
-                "\n" + "Верните JSON-список только тех элементов, в которых были исправлены ошибки, сохраняя " +
-                "структуру объекта. Например:\n" +
-                "\n" + "Входной JSON:\n" +
-                "\n" + "json\n" +
-                "Копировать код\n" + "[\n" +
-                "  {\"value\": \"Мясорубк\", \"rowIndex\": 8, \"columIndex\": 1},\n" +
-                "  {\"value\": \"Телевезор\", \"rowIndex\": 3, \"columIndex\": 1},\n" +
-                "  {\"value\": \"Яблоко\", \"rowIndex\": 4, \"columIndex\": 1}\n" +
-                "]\n" +
-                "Ожидаемый ответ:\n" +
-                "\n" +
-                "json\n" +
-                "Копировать код\n" +
-                "[\n" +
-                "  {\"value\": \"Мясорубка\", \"rowIndex\": 8, \"columIndex\": 1},\n" +
-                "  {\"value\": \"Телевизор\", \"rowIndex\": 3, \"columIndex\": 1}\n" +
+            "Ты - программа-анализатор. Тебе дадут JSON-Объекты, твоя задача определить плохие поля. Под плохим полем" +
+                " подразумевается: цена на товар слишком низкая или высокая, ошибки и логические несостыковки в " +
+                "названиях или описаниях, прочие очевидные ошибки. Обязательно учти, что если в названии заявлен " +
+                "бренд, то в описаниях и бренде должен быть такой же. Если проблема с ценой, предложи свою на основе " +
+                "средней цены за подобные товары. Учти, что каждая ошибка должа быть представлена отдельным объектом, даже если " +
+                "она в одной и той же строке, но разных колонках (полях). Если таких полей нет, то просто верни []. " +
+                "Иначе результат представь в формате JSON со следующей структурой: message: <ОПИСАНИЕ " +
+                "ОШИБКИ>\\nvalue: <НОВОЕ ЗНАЧЕНИЕ ПОЛЯ>\\nrowIndex: <ПОРЯДКОВЫЙ НОМЕР ОБЪЕКТА В JSON>\\ncolumnIndex: " +
+                "<НОМЕР ПОЛЯ ПО СЧЁТУ В ОБЪЕКТЕ>;..." +
                 "]",
             objectMapper.writeValueAsString(items)
         );
-        return yandexGptClient.producePromt(promt);
+
+        var token = yandexIamClient.getToken(new YandexIamClient.IamTokenRequest(oAuthToken)).getIamToken();
+        var authHeader = String.format("Bearer %s", token);
+        var result = yandexGptClient.producePromt(promt, authHeader);
+        var json = result.result().alternatives().get(0).message().text();
+
+        json = json.replaceAll("^```|```$", "");
+
+        Gson gson = new Gson();
+        Type listType = new TypeToken<List<LlmWarnings>>() {}.getType();
+        return gson.fromJson(json, listType);
     }
 }
